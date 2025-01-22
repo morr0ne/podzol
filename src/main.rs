d1332684979e46e48de128042d41855577612b0c
Federico Maria Morrone <contact@morrone.dev>
2025-01-22 21:31:08 +0100

    Start implementing add subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::fs;
+
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 
 mod manifest;
+use manifest::Manifest;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 
 /// Podzol - A modpack manager
 #[derive(Parser)]
@@ -14,14 +19,41 @@ struct Args {
 #[derive(Subcommand)]
 enum Commands {
     Init,
-    Add,
+    Add { r#mod: String },
     Remove,
 }
 
-fn main() -> Result<()> {
+#[derive(Debug, Deserialize, Serialize)]
+struct Version {}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let Args { command } = Args::parse();
 
+    let client = Client::new();
+
     match command {
+        Commands::Add { r#mod } => {
+            let manifest: Manifest = toml_edit::de::from_slice(&fs::read("podzol.toml")?)?;
+
+            // dbg!(manifest);
+
+            let res: Vec<Version> = client
+                .get(format!("https://api.modrinth.com/v2/project/{mod}/version"))
+                .query(&[
+                    ("loaders", format!(r#"["{}"]"#, manifest.pack.loader)),
+                    (
+                        "game_versions",
+                        format!(r#"["{}"]"#, manifest.pack.minecraft),
+                    ),
+                ])
+                .send()
+                .await?
+                .json()
+                .await?;
+
+            println!("Adding..")
+        }
         _ => todo!(),
     }
 
