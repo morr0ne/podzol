a5d595a3ce0dca00b600bc8ab1bef1c98840885c
Federico Maria Morrone <contact@morrone.dev>
2025-01-30 17:44:23 +0100

    Implement xtask pattern

---
+++ xtask/src/main.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+use clap::Parser;
+use xtask_common::{clap, Cli, Empty};
+
+fn main() -> Result<()> {
+    let cli: Cli<Empty> = Cli::parse();
+
+    cli.command.execute("podzol")?;
+
+    Ok(())
+}
